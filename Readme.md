02b9b18b21d833426a22ede7f920ddd90ac5e24d
Marc Guerrini <maguerrini@airfrance.fr>
2021-07-29 13:31:38 +0200

    first commit

---
+++ Readme.md
@@ -0,0 +1,8 @@
+# Getting Started
+
+
+There is a new project type **Mgu**
+
+When trying to create a new project from the STS IDE this type does not show up. :(
+
+When trying to create a new project from the IntelliJ IDE this new type is available. :)
---
+++ src/main/java/com/afklm/tecc/initializer/TeccInitializerApplication.java
@@ -0,0 +1,13 @@
+package com.afklm.tecc.initializer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeccInitializerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeccInitializerApplication.class, args);
+	}
+
+}
